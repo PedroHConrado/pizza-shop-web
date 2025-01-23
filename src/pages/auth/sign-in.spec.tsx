1c446e25bc967bc66faaa0704128833a82a872fa
Pedro Conrado <conradohpedro@gmail.com>
2025-01-23 11:14:58 -0300

    remove unused import

--- src/pages/auth/sign-in.spec.tsx
+++ src/pages/auth/sign-in.spec.tsx
@@ -1,6 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { render } from '@testing-library/react'
-import type { HTMLInputTypeAttribute } from 'react'
 import { HelmetProvider } from 'react-helmet-async'
 import { MemoryRouter } from 'react-router-dom'
 
